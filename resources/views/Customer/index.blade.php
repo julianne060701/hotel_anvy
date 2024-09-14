3d63d9eede788567d38b8eaa8ea5b6c0da85431a
chanie55 <anya.sc30@gmail.com>
2024-09-14 21:34:45 +0800

    update | Home

--- resources/views/Customer/index.blade.php
+++ resources/views/Customer/index.blade.php
@@ -5,6 +5,7 @@
     @include('Customer.components.Footer')
 @endsection
 
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -16,15 +17,16 @@
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-        <link rel="stylesheet" href="vendors/linericon/style.css">
+        <link rel="stylesheet" href="vendors/linericon/style.css"> 
         <link rel="stylesheet" href="css/font-awesome.min.css">
         <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap4-datetimepicker@5.2.3/build/css/bootstrap-datetimepicker.min.css" rel="stylesheet">
         <link rel="stylesheet" href="vendors/nice-select/css/nice-select.css">
         <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
         <!-- main css -->
-        <style> {{ file_get_contents(public_path('css/style.css')) }} </style>
-        <style> {{ file_get_contents(public_path('css/responsive.css')) }} </style>
+        <!-- <style> {{ file_get_contents(public_path('css/style.css')) }} </style> -->
+        <link rel="stylesheet" href="{{ url('css/style.css') }}">
+        <link rel="stylesheet" href="{{ url('css/responsive.css') }}">
     </head>
     <body>
         <!--================Header Area =================-->
@@ -32,14 +34,15 @@
         <!--================Header Area =================-->
         
         <!--================Banner Area =================-->
-        <section class="banner_area">
+        <section class="banner_area" style = "background-image: url(public/images/banner.jpg); background-repeat: no-repeat; background-size: cover;">
+            <!-- <img src="public/images/banner.jpg" alt="image"> -->
             <div class="booking_table d_flex align-items-center">
             	<div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
 				<div class="container">
 					<div class="banner_content text-center">
-						<h6>Away from monotonous life</h6>
-						<h2>Relax Your Mind</h2>
-						<p>If you are looking at blank cassettes on the web, you may be very confused at the<br> difference in price. You may see some for as low as $.17 each.</p>
+						<h6>Relax Your Mind</h6>
+						<h2>Anvy Resort Hotel</h2>
+						<p>Discover the Beauty of Anvy Resort Hotel</p>
 						<a href="#" class="btn theme_btn button_hover">Get Started</a>
 					</div>
 				</div>
@@ -114,19 +117,40 @@
             </div>
         </section>
         <!--================Banner Area =================-->
+
+        <!--================ Welcome Area  =================-->
+        <section class="about_history_area section_gap">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 d_flex align-items-center">
+                        <div class="about_content ">
+                            <h2 class="title title_color">Welcome to Any Resort Hotel</h2>
+                            <p>Now that your're here, let's get started. Let us help you create unforgettable memories.
+                            Whether you seek relaxation by the pool, adventure in our surrounding nature, or culinary delights in our exquisite restaurants, we're here to cater to your every need.
+                            </p>
+                            <!-- <a href="#" class="button_hover theme_btn_two">Request Custom Price</a> -->
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <img class="img-fluid" src="public/images/welcome.jpg" alt="img">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================ Welcome Area  =================-->
         
         <!--================ Accomodation Area  =================-->
         <section class="accomodation_area section_gap">
             <div class="container">
                 <div class="section_title text-center">
-                    <h2 class="title_color">Hotel Accomodation</h2>
+                    <h2 class="title_color">Room and Suites</h2>
                     <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast, </p>
                 </div>
                 <div class="row mb_30">
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room1.jpg" alt="">
+                                <img src="public/images/rooms/room1.jpg" alt="" style="width: 200%; height: 300%;">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Double Deluxe Room</h4></a>
@@ -136,30 +160,10 @@
                     <div class="col-lg-3 col-sm-6">
                         <div class="accomodation_item text-center">
                             <div class="hotel_img">
-                                <img src="image/room2.jpg" alt="">
+                                <img src="public/images/rooms/room2.jpg" alt="" style="width: 200%; height: 300%;">
                                 <a href="#" class="btn theme_btn button_hover">Book Now</a>
                             </div>
                             <a href="#"><h4 class="sec_h4">Single Deluxe Room</h4></a>
-                            <h5>$200<small>/night</small></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="accomodation_item text-center">
-                            <div class="hotel_img">
-                                <img src="image/room3.jpg" alt="">
-                                <a href="#" class="btn theme_btn button_hover">Book Now</a>
-                            </div>
-                            <a href="#"><h4 class="sec_h4">Honeymoon Suit</h4></a>
-                            <h5>$750<small>/night</small></h5>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="accomodation_item text-center">
-                            <div class="hotel_img">
-                                <img src="image/room4.jpg" alt="">
-                                <a href="#" class="btn theme_btn button_hover">Book Now</a>
-                            </div>
-                            <a href="#"><h4 class="sec_h4">Economy Double</h4></a>
                             <h5>$200<small>/night</small></h5>
                         </div>
                     </div>
@@ -167,6 +171,55 @@
             </div>
         </section>
         <!--================ Accomodation Area  =================-->
+
+        <!--================ Menu Area  =================-->
+        <section class="latest_blog_area section_gap">
+            <div class="container">
+                <div class="section_title text-center">
+                    <h2 class="title_color">From our Menus</h2>
+                    <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from </p>
+                </div>
+                <div class="row mb_30">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-recent-blog-post">
+                            <div class="thumb">
+                                <img class="img-fluid" src="public/images/menus/menu1.jpg" alt="post">
+                            </div>
+                            <div class="details">
+                            <h5>$250</h5>
+                                <a href="#"><h4 class="sec_h4">idk Salad</h4></a>
+                                <a href="#" class="btn theme_btn button_hover">Order Now</a>
+                            </div>	
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-recent-blog-post">
+                            <div class="thumb">
+                                <img class="img-fluid" src="public/images/menus/menu2.jpg" alt="post">
+                            </div>
+                            <div class="details">
+                            <h5>$250</h5>
+                                <a href="#"><h4 class="sec_h4">Cheezy Burger</h4></a>
+                                <a href="#" class="btn theme_btn button_hover">Order Now</a>
+                            </div>	
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-recent-blog-post">
+                            <div class="thumb">
+                                <img class="img-fluid" src="public/images/menus/menu3.jpg" alt="post">
+                            </div>
+                            <div class="details">
+                            <h5>$250</h5>
+                                <a href="#"><h4 class="sec_h4">Toast</h4></a>
+                                <a href="#" class="btn theme_btn button_hover">Order Now</a>
+                            </div>	
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================ Menu Area  =================-->
         
         <!--================ Facilities Area  =================-->
         <section class="facilities_area section_gap">
@@ -174,7 +227,7 @@
             </div>
             <div class="container">
                 <div class="section_title text-center">
-                    <h2 class="title_w">Royal Facilities</h2>
+                    <h2 class="title_w">Events</h2>
                     <p>Who are in extremely love with eco friendly system.</p>
                 </div>
                 <div class="row mb_30">
@@ -227,11 +280,11 @@
                         <div class="about_content ">
                             <h2 class="title title_color">About Us <br>Our History<br>Mission & Vision</h2>
                             <p>inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed.</p>
-                            <a href="#" class="button_hover theme_btn_two">Request Custom Price</a>
+                            <a href="#" class="button_hover theme_btn_two">Read More</a>
                         </div>
                     </div>
                     <div class="col-md-6">
-                        <img class="img-fluid" src="image/about_bg.jpg" alt="img">
+                        <img class="img-fluid" src="public/images/about.jpg" alt="img">
                     </div>
                 </div>
             </div>
@@ -247,7 +300,7 @@
                 </div>
                 <div class="testimonial_slider owl-carousel">
                     <div class="media testimonial_item">
-                        <img class="rounded-circle" src="image/testtimonial-1.jpg" alt="">
+                        <img class="rounded-circle" src="public/images/testtimonial-1.jpg" alt="">
                         <div class="media-body">
                             <p>As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face, and She is the </p>
                             <a href="#"><h4 class="sec_h4">Fanny Spencer</h4></a>
@@ -260,113 +313,12 @@
                             </div>
                         </div>
                     </div>    
-                    <div class="media testimonial_item">
-                        <img class="rounded-circle" src="image/testtimonial-1.jpg" alt="">
-                        <div class="media-body">
-                            <p>As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face, and She is the </p>
-                            <a href="#"><h4 class="sec_h4">Fanny Spencer</h4></a>
-                            <div class="star">
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star-half-o"></i></a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="media testimonial_item">
-                        <img class="rounded-circle" src="image/testtimonial-1.jpg" alt="">
-                        <div class="media-body">
-                            <p>As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face, and She is the </p>
-                            <a href="#"><h4 class="sec_h4">Fanny Spencer</h4></a>
-                            <div class="star">
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star-half-o"></i></a>
-                            </div>
-                        </div>
-                    </div>    
-                    <div class="media testimonial_item">
-                        <img class="rounded-circle" src="image/testtimonial-1.jpg" alt="">
-                        <div class="media-body">
-                            <p>As conscious traveling Paupers we must always be concerned about our dear Mother Earth. If you think about it, you travel across her face, and She is the </p>
-                            <a href="#"><h4 class="sec_h4">Fanny Spencer</h4></a>
-                            <div class="star">
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star"></i></a>
-                                <a href="#"><i class="fa fa-star-half-o"></i></a>
-                            </div>
-                        </div>
-                    </div>
                 </div>
             </div>
         </section>
         <!--================ Testimonial Area  =================-->
         
-        <!--================ Latest Blog Area  =================-->
-        <section class="latest_blog_area section_gap">
-            <div class="container">
-                <div class="section_title text-center">
-                    <h2 class="title_color">latest posts from blog</h2>
-                    <p>The French Revolution constituted for the conscience of the dominant aristocratic class a fall from </p>
-                </div>
-                <div class="row mb_30">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="single-recent-blog-post">
-                            <div class="thumb">
-                                <img class="img-fluid" src="image/blog/blog-1.jpg" alt="post">
-                            </div>
-                            <div class="details">
-                                <div class="tags">
-                                    <a href="#" class="button_hover tag_btn">Travel</a>
-                                    <a href="#" class="button_hover tag_btn">Life Style</a>
-                                </div>
-                                <a href="#"><h4 class="sec_h4">Low Cost Advertising</h4></a>
-                                <p>Acres of Diamonds… you’ve read the famous story, or at least had it related to you. A farmer.</p>
-                                <h6 class="date title_color">31st January,2018</h6>
-                            </div>	
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="single-recent-blog-post">
-                            <div class="thumb">
-                                <img class="img-fluid" src="image/blog/blog-2.jpg" alt="post">
-                            </div>
-                            <div class="details">
-                                <div class="tags">
-                                    <a href="#" class="button_hover tag_btn">Travel</a>
-                                    <a href="#" class="button_hover tag_btn">Life Style</a>
-                                </div>
-                                <a href="#"><h4 class="sec_h4">Creative Outdoor Ads</h4></a>
-                                <p>Self-doubt and fear interfere with our ability to achieve or set goals. Self-doubt and fear are</p>
-                                <h6 class="date title_color">31st January,2018</h6>
-                            </div>	
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6">
-                        <div class="single-recent-blog-post">
-                            <div class="thumb">
-                                <img class="img-fluid" src="image/blog/blog-3.jpg" alt="post">
-                            </div>
-                            <div class="details">
-                                <div class="tags">
-                                    <a href="#" class="button_hover tag_btn">Travel</a>
-                                    <a href="#" class="button_hover tag_btn">Life Style</a>
-                                </div>
-                                <a href="#"><h4 class="sec_h4">It S Classified How To Utilize Free</h4></a>
-                                <p>Why do you want to motivate yourself? Actually, just answering that question fully can </p>
-                                <h6 class="date title_color">31st January,2018</h6>
-                            </div>	
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--================ Recent Area  =================-->
+        
         
         <!--================ start footer Area  =================-->	
         @yield('Footer')
