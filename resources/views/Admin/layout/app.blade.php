7ceb6d943c9e6a7074b341f94de56f83d20baebd
Ann <fernandez.226952@gensan.sti.edu.ph>
2024-09-16 23:10:15 +0800

    admin sidebar not function

--- resources/views/Admin/layout/app.blade.php
+++ resources/views/Admin/layout/app.blade.php
@@ -30,6 +30,16 @@
 
   <!--Data Table-->
   <link rel='stylesheet' href='https://cdn.datatables.net/2.1.5/css/dataTables.dataTables.min.css'>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="resources/plugins/fontawesome-free/css/all.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="resources/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="resources/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="resources/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -127,6 +137,19 @@
 <script src='https://cdn.datatables.net/2.1.5/js/dataTables.min.js'></script>
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <script src="resources/dist/js/pages/dashboard.js"></script>
+<!-- DataTables  & Plugins -->
+<script src="resources/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="resources/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="resources/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="resources/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="resources/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="resources/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="resources/plugins/jszip/jszip.min.js"></script>
+<script src="resources/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="resources/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="resources/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="resources/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="resources/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 
 @yield('scripts');
 </body>
