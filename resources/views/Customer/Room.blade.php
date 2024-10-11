f8d25815b17e91442904422c0f6e1eabe711e7ae
Ann <fernandez.226952@gensan.sti.edu.ph>
2024-10-11 21:31:52 +0800

    rooms UI customer

--- resources/views/Customer/Room.blade.php
+++ resources/views/Customer/Room.blade.php
@@ -6,8 +6,6 @@
 @include('Customer.components.Footer')
 @endsection
 
-
-
 <body>
     <!--================Navigations Area =================-->
     @yield('Navbar')
@@ -27,50 +25,71 @@
         </div>
     </section>
     <!--================Breadcrumb Area =================-->
- <!--================ Accommodation Area  =================-->
- <section class="accommodation_area section_gap">
+
+    <!--================ Accommodation Area  =================-->
+    <section class="accommodation_area section_gap">
         <div class="container">
-            <div class="section_title text-center">
-                <h2 class="title_color">Room and Suites</h2>
-                <p>We all live in an age that belongs to the young at heart. Life that is becoming extremely fast,</p>
-            </div>
-            <div class="row mb_30">
-                <div class="col-lg-3 col-sm-6">
-                    <div class="accommodation_item text-center">
-                        <div class="hotel_img">
-                            <img src="public/images/rooms/room1.jpg" alt="Double Deluxe Room">
-                            <a href="#" class="btn theme_btn button_hover">See Details</a>
+        <div class="section_title text-center">
+    <h2 class="title_bold">Room and Suites</h2>
+    <p class="subtext">We all live in an age that belongs to the young at heart. Life that is becoming extremely fast,</p>
+</div>
+
+
+            <div class="row">
+                <!-- Card 1 -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="card accommodation_item" data-wow-delay="0.1s">
+                        <img src="public/images/rooms/room1.jpg" class="card-img-top" alt="Double Deluxe Room">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Junior Suite</h5>
+                            <p class="card-text"><i class="fa fa-bed"></i> 3 Bed &nbsp; <i class="fa fa-bath"></i> 2
+                                Bath &nbsp; <i class="fa fa-wifi"></i> Wifi</p>
+                            <h5>₱250<small>/night</small></h5>
+                            <a href="#" class="btn btn-primary">View Detail</a>
+                            <a href="#" class="btn btn-secondary">Book Now</a>
                         </div>
-                        <a href="#">
-                            <h4 class="sec_h4">Double Deluxe Room</h4>
-                        </a>
-                        <h5>₱250<small>/night</small></h5>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-sm-6">
-                    <div class="accommodation_item text-center">
-                        <div class="hotel_img">
-                            <img src="public/images/rooms/room2.jpg" alt="Single Deluxe Room">
-                            <a href="#" class="btn theme_btn button_hover">See Details </a>
-                        </div>
-                        <a href="#">
-                            <h4 class="sec_h4">Single Deluxe Room</h4>
-                        </a>
-                        <h5>₱200<small>/night</small></h5>
                     </div>
                 </div>
 
+                <!-- Card 2 -->
+                <div class="col-lg-4 col-md-6">
+                    <div class="card accommodation_item" data-wow-delay="0.1s">
+                        <img src="public/images/rooms/room2.jpg" class="card-img-top" alt="Single Deluxe Room">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Executive Suite</h5>
+                            <p class="card-text"><i class="fa fa-bed"></i> 3 Bed &nbsp; <i class="fa fa-bath"></i> 2
+                                Bath &nbsp; <i class="fa fa-wifi"></i> Wifi</p>
+                            <h5>₱200<small>/night</small></h5>
+                            <a href="#" class="btn btn-primary">View Detail</a>
+                            <a href="#" class="btn btn-secondary">Book Now</a>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Card 3 -->
+                <div class="col-lg-4 col-md-6 " data-wow-delay="0.1s">
+                    <div class="card accommodation_item">
+                        <img src="public/images/rooms/event3.jpg" class="card-img-top" alt="Super Deluxe Room">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Super Deluxe</h5>
+                            <p class="card-text"><i class="fa fa-bed"></i> 3 Bed &nbsp; <i class="fa fa-bath"></i> 2
+                                Bath &nbsp; <i class="fa fa-wifi"></i> Wifi</p>
+                            <h5>₱300<small>/night</small></h5>
+                            <a href="#" class="btn btn-primary">View Detail</a>
+                            <a href="#" class="btn btn-secondary">Book Now</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
     <!--================ Accommodation Area  =================-->
+
     <!--================ start footer Area  =================-->
     @yield('Footer')
     <!--================ End footer Area  =================-->
 
-
     <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
         integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
         crossorigin="anonymous"></script>
@@ -80,14 +99,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
         integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
         crossorigin="anonymous"></script>
-    <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
-    <script src="js/jquery.ajaxchimp.min.js"></script>
-    <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
-    <script src="vendors/nice-select/js/jquery.nice-select.js"></script>
-    <script src="js/mail-script.js"></script>
-    <script src="js/stellar.js"></script>
-    <script src="vendors/lightbox/simpleLightbox.min.js"></script>
-    <script src="js/custom.js"></script>
 </body>
 
 </html>
